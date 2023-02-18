f74a654672a3ab4648b7c32db1b9cb4bdf79c99a
marokoro7636 <64199605+marokoro7636@users.noreply.github.com>
2023-02-18 18:23:42 +0900

    Add info on the order list user screen

--- lib/ui/order_list_user/order_list_user_screen.dart
+++ lib/ui/order_list_user/order_list_user_screen.dart
@@ -1,4 +1,5 @@
 import 'package:customer_management/ui/order_list_user/order_list_user_viewmodel.dart';
+import 'package:customer_management/util/ext.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -36,6 +37,8 @@ class _OrderListUserPage extends HookConsumerWidget {
         return Card(
           child: ListTile(
             title: Text(order.goodsName),
+            subtitle: Text('注文日:${order.orderDate!.toFormattedString()}'),
+            trailing: Text('￥${order.goodsPrice} x ${order.goodsAmount}'),
             onTap: () => viewModel.navigateOrderInfoScreen(context, index),
           ),
         );
