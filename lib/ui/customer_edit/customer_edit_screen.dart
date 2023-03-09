bbab7238c8bd14aa2359fe870654f109d067cdee
alpherg0221 <78247593+alpherg0221@users.noreply.github.com>
2023-03-09 14:17:12 +0900

    Added validation

--- lib/ui/customer_edit/customer_edit_screen.dart
+++ lib/ui/customer_edit/customer_edit_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:customer_management/ui/customer_edit/customer_edit_viewmodel.dart';
 
-
 class CustomerEditScreen extends HookConsumerWidget {
   CustomerEditScreen({Key? key}) : super(key: key);
-
-  final globalKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,111 +19,99 @@ class CustomerEditScreen extends HookConsumerWidget {
         child: Container(
           padding: const EdgeInsets.all(30),
           child: Form(
-              key: globalKey,
+              key: viewModel.globalKey,
               child: Column(
                 children: [
                   const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.name,
+                    //initialValue: state.customer.name,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: '氏名',
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '氏名を入力してください';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) => viewModel.setName(value),
+                    validator: viewModel.validateName,
+                    onChanged: viewModel.setName,
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.nameKana,
+                    //initialValue: state.customer.nameKana,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: '氏名(ひらがな)',
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '氏名(ひらがな)を入力してください';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) => viewModel.setNameKana(value),
-                  ),const SizedBox(height: 20),
+                    inputFormatters: [
+                      TextInputFormatter.withFunction((oldValue, newValue) =>
+                          RegExp(r'^[ぁ-ん]*$').hasMatch(newValue.text)
+                              ? newValue
+                              : oldValue),
+                    ],
+                    validator: viewModel.validateNameKana,
+                    onChanged: viewModel.setNameKana,
+                  ),
+                  const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.postCode,
+                    //initialValue: state.customer.postCode,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: '郵便番号',
+                      counterText: '',
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        // TODO 郵便番号用のバリデーション
-                        return '郵便番号を入力してください';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) => viewModel.setPostCode(value),
-                  ),const SizedBox(height: 20),
+                    maxLength: 7,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: viewModel.validatePostCode,
+                    onChanged: viewModel.setPostCode,
+                  ),
+                  const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.address,
+                    //initialValue: state.customer.address,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: '住所',
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '住所を入力してください';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) => viewModel.setAddress(value),
-                  ),const SizedBox(height: 20),
+                    validator: viewModel.validateAddress,
+                    onChanged: viewModel.setAddress,
+                  ),
+                  const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.accountName,
+                    //initialValue: state.customer.accountName,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'アカウント名',
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'アカウント名を入力してください';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) => viewModel.setAccountName(value),
-                  ),const SizedBox(height: 20),
+                    validator: viewModel.validateAccountName,
+                    onChanged: viewModel.setAccountName,
+                  ),
+                  const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.accountId,
+                    //initialValue: state.customer.accountId,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'アカウントID(英数字記号)',
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'アカウントIDを入力してください';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) => viewModel.setAccountId(value),
-                  ),const SizedBox(height: 20),
+                    inputFormatters: [
+                      TextInputFormatter.withFunction((oldValue, newValue) =>
+                          RegExp(r'^\p{ASCII}*$', unicode: true)
+                                  .hasMatch(newValue.text)
+                              ? newValue
+                              : oldValue),
+                    ],
+                    validator: viewModel.validateAccountId,
+                    onChanged: viewModel.setAccountId,
+                  ),
+                  const SizedBox(height: 20),
                   TextFormField(
-                    initialValue: state.customer.notes,
+                    //initialValue: state.customer.notes,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: '備考',
                     ),
-                    onChanged: (value) => viewModel.setNotes(value),
+                    onChanged: viewModel.setNotes,
                   ),
                   const SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (globalKey.currentState!.validate()) {
-                          viewModel.save(context);
-                          // TODO 保存ダイアログを出す
-                        }
+                  FilledButton(
+                      onPressed: () async {
+                        await viewModel.save(context).then((value) => null);
                       },
                       child: const Text('保存'))
                 ],
--- lib/ui/customer_edit/customer_edit_viewmodel.dart
+++ lib/ui/customer_edit/customer_edit_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:customer_management/model/entity/customer.dart';
 import 'package:customer_management/ui/customer_edit/customer_edit_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,41 +14,93 @@ class CustomerEditViewModel extends StateNotifier<CustomerEditState> {
 
   final customerRepository = CustomerRepository(AppDatabase());
 
+  final globalKey = GlobalKey<FormState>();
+
   void setName(String value) {
     state = state.copyWith(customer: state.customer.copyWith(name: value));
+  }
+
+  String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return '氏名を入力してください';
+    }
+    return null;
   }
 
   void setNameKana(String value) {
     state = state.copyWith(customer: state.customer.copyWith(nameKana: value));
   }
 
+  String? validateNameKana(String? value) {
+    if (value == null || value.isEmpty) {
+      return '氏名(ひらがな)を入力してください';
+    }
+    return null;
+  }
+
   void setPostCode(String value) {
     state = state.copyWith(customer: state.customer.copyWith(postCode: value));
+  }
+
+  String? validatePostCode(String? value) {
+    if (value == null || value.isEmpty) {
+      return '郵便番号を入力してください';
+    }
+    if (value.length != 7) {
+      return '郵便番号は7桁です';
+    }
+    return null;
   }
 
   void setAddress(String value) {
     state = state.copyWith(customer: state.customer.copyWith(address: value));
   }
 
+  String? validateAddress(String? value) {
+    if (value == null || value.isEmpty) {
+      return '住所を入力してください';
+    }
+    return null;
+  }
+
   void setAccountName(String value) {
-    state = state.copyWith(customer: state.customer.copyWith(accountName: value));
+    state =
+        state.copyWith(customer: state.customer.copyWith(accountName: value));
+  }
+
+  String? validateAccountName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'アカウント名を入力してください';
+    }
+    return null;
   }
 
   void setAccountId(String value) {
     state = state.copyWith(customer: state.customer.copyWith(accountId: value));
   }
 
+  String? validateAccountId(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'アカウントIDを入力してください';
+    }
+    return null;
+  }
+
   void setNotes(String value) {
     state = state.copyWith(customer: state.customer.copyWith(notes: value));
   }
 
-  void save(BuildContext context) async {
-    if (state.addMode) {
-      await customerRepository.insert(state.customer);
-    } else {
-      await customerRepository
-          .update(state.customer)
-          .then((_) => Navigator.pop(context, state.customer));
+  Future save(BuildContext context) async {
+    if (globalKey.currentState!.validate()) {
+      if (state.addMode) {
+        await customerRepository.insert(state.customer);
+        state = state.copyWith(customer: const Customer());
+        globalKey.currentState!.reset();
+      } else {
+        await customerRepository
+            .update(state.customer)
+            .then((_) => Navigator.pop(context, state.customer));
+      }
     }
   }
 }
